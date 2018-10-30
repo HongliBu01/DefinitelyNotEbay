1e7eebf787d42bf65aa51104a53727e3e4f7aee6
Josephine-Chen <jychen@g.hmc.edu>
2018-10-29 19:08:47 -0500

    Connect mongodb

--- server.py
+++ server.py
@@ -2,8 +2,28 @@
 # -*- coding: utf-8 -*-
 
 import os
+import json
+import datetime
+import config
+from bson.objectid import ObjectId
 from flask import Flask, render_template
+from flask_pymongo import PyMongo
+
+class JSONEncoder(json.JSONEncoder):
+    ''' extend json-encoder class'''
+
+    def default(self, o):
+        if isinstance(o, ObjectId):
+            return str(o)
+        if isinstance(o, datetime.datetime):
+            return str(o)
+        return json.JSONEncoder.default(self, o)
+
 app = Flask(__name__)
+
+# add mongo url to flask config, so that flask_pymongo can use it to make connection
+app.config['MONGO_URI'] = config.DB
+mongo = PyMongo(app)
 
 
 @app.route("/")
